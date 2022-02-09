c7eeb367d5411778c4e5b8363ad52b95ebfb3b76
Alexis <yernauxalex@gmail.com>
2022-02-09 11:17:40 +0100

    Add signin + fix user method

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,9 +5,8 @@ const db = require('../database')
 // Création d'un nouveau compte
 exports.signup = async (req, res) => {
     try{
-        const email = req.body.email;
         const passwordh = await bcrypt.hash(req.body.password, 10);
-        await db.any("INSERT INTO users (email, passwordh) VALUES ($1,$2);", [req.body.email, passwordh]);
+        await db.any("INSERT INTO users (email, firstname, lastname, passwordh) VALUES ($1,$2,$3,$4);", [req.body.email, req.body.lastname, req.body.firstname, passwordh]);
         return res.status(201).json({ message: 'Utilisateur crée' });
         }
     catch(error) {
@@ -19,6 +18,9 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
     try {
         const password = req.body.password;
+        console.log("before sql request - email: ",req.body.email)
+        console.log("before sql request - password: ",req.body.password)
+
         const data = await db.any("SELECT id_user, email, passwordh FROM users WHERE email = $1", req.body.email)
         const id_user = data[0].id_user;
         const emaildb = data[0].email;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,82 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+// Appel API
+async function fetchSignup(credentials) {
+  return fetch('http://localhost:3000/api/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json())
+}
+
 function Signup() {
-    return (
-        <div>
-            <h1>Inscription</h1>
+  const [email, setEmail] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [firstname, setFirstname] = useState('')
+  const [password, setPassword] = useState('')
+  let navigate = useNavigate()
+
+  // Fonction appelée lors du submit
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await fetchSignup({
+      email,
+      lastname,
+      firstname,
+      password,
+    })
+    navigate('/Signin')
+  }
+
+  return (
+    <div>
+      <h1>Inscription</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Email </label>
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
-    )
+        <div className="input-container">
+          <label>Nom </label>
+          <input
+            type="text"
+            name="lastname"
+            onChange={(e) => setLastname(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Prénom </label>
+          <input
+            type="text"
+            name="firstname"
+            onChange={(e) => setFirstname(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label>Mot de passe </label>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Signup
