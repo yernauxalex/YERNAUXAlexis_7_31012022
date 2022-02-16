a9c0b1edccf775ff4edc53885c3be79cd4ebd709
Alexis <yernauxalex@gmail.com>
2022-02-16 14:10:15 +0100

    Add local db

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // PostgreSQL
-const db = pgp(process.env.POSTGRESQL);
+// Hosted db
+//const db = pgp(process.env.POSTGRESQL);
+// Local db
+const db = pgp(process.env.POSTGRESQLLOCAL);
 db.connect()
 .then(() => console.log('Connected to ElephantSQL'))
 .catch(() => console.log('Connection failed'));
