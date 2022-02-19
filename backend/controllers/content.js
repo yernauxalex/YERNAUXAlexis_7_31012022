598f6d41e848360d4e722457cef4308c8b65f98b
Alexis <yernauxalex@gmail.com>
2022-02-19 11:42:23 +0100

    Add resize img

--- backend/controllers/content.js
+++ backend/controllers/content.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const sharp = require('sharp');
+const path = require('path');
 const db = require('../database');
 
 // Création d'un nouveau contenu dans la DB
@@ -7,14 +9,19 @@ exports.createContent = async (req, res, next) => {
         let media, media_content;
         if(req.body.media == 'true'){
             media = true;
-            media_content = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+            const { filename: file } = req.file;
+            const resizedPic = await sharp(req.file.path).resize(504, 504, {
+                fit: 'outside'
+            }).toFile(path.resolve(req.file.destination,'resized' + req.file.filename))
+            fs.unlinkSync(req.file.path)
+            media_content = `${req.protocol}://${req.get('host')}/images/${'resized' + req.file.filename}`
         }
         else{
             media = false;
         }
         const data = await db.one("INSERT INTO content (text_content, media, media_content, id_author) VALUES ($1,$2,$3,$4) RETURNING id_content",[req.body.text_content, media, media_content, req.params.id_user]);
         const update = await db.any("UPDATE users SET last_interaction = $1, interaction = 'content' WHERE id_user = $2", [data.id_content, req.params.id_user]);
-        return res.status(201).json({ message: 'Contenu ajouté' });
+        return res.status(201).json({ message: 'Contenu ajouté' , last_interaction: data.id_content});
     }
     catch(error){
         console.log(error)
@@ -25,7 +32,7 @@ exports.createContent = async (req, res, next) => {
 // Recherche du contenu récent
 exports.getRecentContent = async (req, res, next) => {
     try{
-        const data = await db.any("SELECT * FROM content ORDER BY date DESC LIMIT 5");
+        const data = await db.any("SELECT * FROM content ORDER BY date DESC, id_content DESC LIMIT 5");
         let datajson = [];
         for (let index = 0; index < 10; index++) {
             if (data[index] != null){
@@ -43,7 +50,7 @@ exports.getRecentContent = async (req, res, next) => {
 // Recherhe d'un contenu par son id 
 exports.getOneContent = async (req, res, next) => {
     try{
-        const data = await db.any("SELECT * FROM content WHERE id_content = $1", req.params.id);
+        const data = await db.any("SELECT * FROM content WHERE id_content = $1 LIMIT 1", req.params.id);
         if (req.params.id != data[0].id_content){
             return res.status(401).json({ error: 'Contenu non trouvé' });
         }
