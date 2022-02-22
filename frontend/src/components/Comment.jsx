d58d0c1469184c10b53043b51694581de1b157c9
Alexis <yernauxalex@gmail.com>
2022-02-22 08:37:09 +0100

    WIP access to other user profile

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
+import { StyledLinkProfile } from '../styles/styledComponent'
 
-// Bouton pour la création d'un commentaire à ajouter
-
-const StyledComment = styled.div`
-  display: flex;
-  flex-flow: colum wrap;
-  flex-direction: column;
-`
 function Comment(props) {
   return (
     <React.Fragment>
       <h4>
-        Commentaire de {props.firstname} {props.lastname}
+        <StyledLinkProfile to={`/PublicProfile/${props.id_author_comment}`}>
+          Commentaire de {props.firstname} {props.lastname}
+        </StyledLinkProfile>
       </h4>
       <p>comment{props.data_comment}</p>
     </React.Fragment>
